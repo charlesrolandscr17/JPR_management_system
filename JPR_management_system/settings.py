5287f772d77460c17eb2b452a6bf0762270d4b8c
Charles Roland <charlesrolandscr17@gmail.com>
2023-01-28 19:54:22 +0300

    Database

--- JPR_management_system/settings.py
+++ JPR_management_system/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['*']
+# CSRF_TRUSTED_ORIGINS = ['*']
 
 
 # Application definition
