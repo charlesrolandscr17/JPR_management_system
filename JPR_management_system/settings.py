adf35e45e07adbce68c8a46b275c973ba1229aca
Charles Roland <charlesrolandscr17@gmail.com>
2023-01-28 19:21:57 +0300

    Database

--- JPR_management_system/settings.py
+++ JPR_management_system/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'JPR_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': config("ENGINE"),
         'NAME': config("NAME"),
         "USER": config("USER"),
         "PASSWORD": config("PASSWORD"),
