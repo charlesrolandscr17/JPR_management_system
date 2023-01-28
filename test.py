e6e07a38a58c203b8d6faa83f49a9ec923fd750c
Charles Roland <charlesrolandscr17@gmail.com>
2023-01-28 20:13:41 +0300

    Database

--- test.py
+++
@@ -1,9 +0,0 @@
-"""
-This is a test python file. By tests I do not mean testing the application but testing
-the ability of a code snippet to work
-"""
-import datetime
-
-print(datetime.datetime.now())
-
-print("Hello World")
