1abf885a3784fc1a0ec84662f55147b81bf0f4d8
Charles Roland <charlesrolandscr17@gmail.com>
2023-01-31 19:54:29 +0300

    Deploy

--- JPR_management_system/wsgi.py
+++ JPR_management_system/wsgi.py
@@ -14,4 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'JPR_management_system.settings')
 
-application = application
+app = application
